c458227f7a500254de6f871ba0bb093ce06bc0d4
Michael Pabody <michael.pabody@gmail.com>
2019-08-27 18:01:02 -0400

    Add project files.

---
+++ MVC_GeneralStore/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_GeneralStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set;}
+
+        [Required]
+        [Display (Name="Product Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Display (Name="# in stock")]
+        public int InventoryCount { get; set; }
+
+        [Required]
+        [Display (Name="It Is Food")]
+        public bool IsFood { get; set; }
+    }
+}
---
+++ MVC_GeneralStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_GeneralStore.Startup))]
+namespace MVC_GeneralStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
