bf582c9deed8dc7c1d1bc9227df9ddcf94f90196
Michael Pabody <michael.pabody@gmail.com>
2019-08-28 17:37:19 -0400

    customer controller methods built

--- MVC_GeneralStore/Controllers/CustomerController.cs
+++ MVC_GeneralStore/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -39,6 +40,80 @@ namespace MVC_GeneralStore.Controllers
             return View(customer);
         }
 
+        // GET : Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _dbCustomer.Customers.Find(id);
+            if(customer == null)
+            {
+                return (HttpNotFound());
+            }
+            return View(customer);
+        }
 
+        // POST : Customer/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _dbCustomer.Customers.Find(id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            _dbCustomer.Customers.Remove(customer);
+            _dbCustomer.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET : Customer/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _dbCustomer.Customers.Find(id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST : Customer/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbCustomer.Entry(customer).State = EntityState.Modified;
+                _dbCustomer.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET : Customer/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _dbCustomer.Customers.Find(id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
     }
 }
